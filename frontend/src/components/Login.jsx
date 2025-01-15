530db34bc237af7501e5b8fde6c5c41c92e99dbd
isak-kang <wbsldj59@naver.com>
2025-01-15 16:25:05 +0900

    폰트 수정.. password 위해

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -55,10 +55,10 @@ const Login = () => {
                       color: "#000",
                       backgroundColor: "#fff",
                       fontFamily: "Arial, Helvetica, sans-serif !important", // 기본 글씨체로 설정
-                    }}>로그인</h2>
+                    }}>로그인1223</h2>
                     <h2
-                      className="login-input">로그인</h2>
-                    <h2>로그인</h2>
+                      className="login-input">로그인12323</h2>
+                    <h2>로그인123</h2>
                   </td>
                 </tr>
                 <tr>
@@ -115,7 +115,7 @@ const Login = () => {
                     <Link to="/id_search">아이디 찾기</Link>
                   </td>
                   <td className="id_search">
-                  <Link to="/password_update">비밀번호 재설정</Link>
+                  <Link to="/password_update">비밀번호 재설정!!</Link>
                   </td>
                 </tr>
               </tbody>
