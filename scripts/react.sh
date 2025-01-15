3ac5e9707fc56dc29cdc6805f6697a75e3d79dba
isak-kang <wbsldj59@naver.com>
2025-01-15 16:30:13 +0900

    오류수정

--- scripts/react.sh
+++ scripts/react.sh
@@ -9,7 +9,6 @@ npm install vite --save-dev
 
 cat ../../env_creat > .env
 
-npm run build
 
 sudo cp -r /home/ubuntu/lgu_final_project/lgu_final/frontend/dist/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html
