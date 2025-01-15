cfc31fefde06981b631fe541b5e62864ff0a6f70
isak-kang <wbsldj59@naver.com>
2025-01-15 12:10:19 +0900

    사용자 정보

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -45,34 +45,6 @@ const Main = () => {
     setModalData(null);
   };
 
-  useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      axios
-        .get(`http://${API_URL}/api/protected`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          setUser(response.data.user);
-        })
-        .catch((error) => {
-          console.error("Authorization failed:", error);
-          if (error.response?.status === 401) {
-            alert("Session expired. Please log in again.");
-            localStorage.removeItem("access_token");
-            setUser(null);
-            window.location.href = "/login";
-          }
-        });
-    } else {
-      setUser(null);
-    }
-  }, []);
-
-
-
   const handleLogout = async () => {
     const token = localStorage.getItem("access_token");
     if (token) {
